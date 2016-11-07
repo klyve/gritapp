73833eb6d71cfd9b056e37e1c63bc93adf47611c
Jørgen Hanssen <jorkiz97@mail.com>
2016-11-07 10:23:47 +0100

    made the testdata on friends more intuative

--- app/components/Friends.js
+++ app/components/Friends.js
@@ -22,31 +22,27 @@ var me = new person();
   me.name = "Jørgen";
   me.picturePath = "http://facebookcraze.com/wp-content/uploads/2009/12/funny_profile_pic_for_facebook_rape.jpg";
 
-var friend1 = new person();
-  friend1.name = "Atbin";
-  friend1.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/13450786_1166169403435324_6701603115698877888_n.jpg?oh=8deb29a29e5d8a0a74a7bf5527317fea&oe=588AD4EE";
+var f1 = new person();
+  f1.name = "Atbin";
+  f1.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/13450786_1166169403435324_6701603115698877888_n.jpg?oh=8deb29a29e5d8a0a74a7bf5527317fea&oe=588AD4EE";
 
-var friend2 = new person();
-  friend2.name = "Bjarte";
-  friend2.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/534545_10151000508639230_719468928_n.jpg?oh=2dfc2295927eb46651e733279090a26d&oe=589533F5";
+var f2 = new person();
+  f2.name = "Bjarte";
+  f2.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/534545_10151000508639230_719468928_n.jpg?oh=2dfc2295927eb46651e733279090a26d&oe=589533F5";
 
-  var friend3 = new person();
-    friend3.name = "Henrik";
-    friend3.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/1484606_10204683850909326_7450466084526933181_n.jpg?oh=36174d2356a1d6d7c8685bcd4c367103&oe=589633B4";
+var f3 = new person();
+  f3.name = "Henrik";
+  f3.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/1484606_10204683850909326_7450466084526933181_n.jpg?oh=36174d2356a1d6d7c8685bcd4c367103&oe=589633B4";
 
-var friend4 = new person();
-  friend4.name = "Karoline";
-  friend4.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/12541098_1218109328203241_3639725756279389260_n.jpg?oh=d72325b4e0cb78a1baddbe70d3e2950a&oe=58D1DCDC";
+var f4 = new person();
+  f4.name = "Karoline";
+  f4.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/12541098_1218109328203241_3639725756279389260_n.jpg?oh=d72325b4e0cb78a1baddbe70d3e2950a&oe=58D1DCDC";
 
-var friend5 = new person();
-  friend5.name = "Morten";
-  friend5.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/11892209_1180715341944580_8031166336427477610_n.jpg?oh=bc73eba1c5d4c2815d3f2d4f464c251a&oe=58CD8ADD";
+var f5 = new person();
+  f5.name = "Morten";
+  f5.picturePath = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/11892209_1180715341944580_8031166336427477610_n.jpg?oh=bc73eba1c5d4c2815d3f2d4f464c251a&oe=58CD8ADD";
 
-me.friends.push(friend1);
-me.friends.push(friend2);
-me.friends.push(friend3);
-me.friends.push(friend4);
-me.friends.push(friend5);
+me.friends = [f1,f2,f3,f4,f5];
 
 export default class Friends extends Component {
   render() {
