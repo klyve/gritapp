abf488482402c93d803ed878914d0d94c3fda111
Morten <morten@omholt-jensen.com>
2016-11-16 22:58:35 +0100

    fikset style profile settings

--- app/components/myprofile/styles/ProfileSettingsMain.js
+++ app/components/myprofile/styles/ProfileSettingsMain.js
@@ -40,13 +40,16 @@ export default styles = StyleSheet.create({
   },
   settingsContainer: {
     backgroundColor: 'white',
+    flex: .7,
   },
   setting: {
+    flex: .3,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
     paddingLeft: 15,
     backgroundColor: '#ffffff',
-    flexDirection: 'row',
-    height: 60,
+    height: 50,
     borderColor: 'rgba(0,0,0,0.3)',
   },
   settingsTextLeft: {
@@ -55,16 +58,12 @@ export default styles = StyleSheet.create({
     width: 200,
   },
   settingsTextRight: {
+    width: 180,
     color: 'grey',
     fontSize: 18,
-    width: 100,
-  },
-  settingsButton: {
-    width: 20,
-    height: 20,
-    marginLeft: 20,
   },
   switch: {
-    marginLeft: 100,
+    width: 200,
+    marginLeft: 90,
   },
 })
