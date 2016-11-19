7e9b9e925ec8a0ceaa9830da2ba2fb246c22d4ea
Hanssen97 <jorkiz97@gmail.com>
2016-11-19 21:28:13 +0100

    Jeg aner faktisk ikke hva jeg har gjort her som er anderledes, så dette er en comitt så jeg får sett det. positivt: +1 commit <3

--- app/components/challenge/ChallengeCamera.js
+++ app/components/challenge/ChallengeCamera.js
@@ -154,7 +154,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    bottom: 24,
+    bottom: 0,
     left: 0,
     right: 0,
     opacity: 0.4,
